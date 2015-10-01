42bd871399047812013918013144c9f993957834
Ana Nelson <anaclairnelson@gmail.com>
2015-10-01 14:33:00 -0700

    Add filter functionality and remove show route

--- todo_list/app/controllers/tasks_controller.rb
+++ todo_list/app/controllers/tasks_controller.rb
@@ -1,15 +1,23 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle_complete]
+  before_action :set_task, only: [:edit, :update, :destroy, :toggle_complete]
+  before_action :order_tasks, only: [:index, :filter]
 
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.order(created_at: :desc)
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
+  # GET /tasks/filter/:filter
+  # GET /tasks/filter/:filter.json
+  def filter
+    filter = params[:filter]
+    filters = {
+      "all" => @tasks,
+      "active" => @tasks.where(completed: false),
+      "completed" => @tasks.where(completed: true)
+    }
+    @tasks = filters[filter]
+    render 'index'
   end
 
   # GET /tasks/new
@@ -77,6 +85,10 @@ class TasksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
+    end
+
+    def order_tasks
+      @tasks = Task.order(created_at: :desc)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
